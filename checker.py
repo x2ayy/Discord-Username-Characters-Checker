ee1e5e3fd738af1618ca0cc9f4c7cfa10f7f262c
Jake <112898008+clientlessx@users.noreply.github.com>
2023-06-24 21:07:27 +0200

    Update to support users who have already updated their username

--- checker.py
+++ checker.py
@@ -1,11 +1,11 @@
 from random import sample as rand_sample
 from string import ascii_letters
-from requests import post as req_post
+from requests import patch as req_patch
 from time import sleep
 from colorama import Fore
 
 
-BASE_URL = "https://discord.com/api/v9/users/@me/pomelo-attempt"
+BASE_URL = "https://discord.com/api/v9/users/@me"
 
 REQUEST_HEADERS = {
     "Content-Type": "Application/json",
@@ -21,20 +21,24 @@ def check_username(user: str):
     if 1 > len(user) < 33:
         print("Username must be 2-32 Characters")
         return
+    
+    user = user.lower()
         
     data = {
         "username": user
     }
-    check = req_post(BASE_URL, headers=REQUEST_HEADERS, json=data)
-    
+    check = req_patch(BASE_URL, headers=REQUEST_HEADERS, json=data)
+    checked_user = check.json()
     try:
-        if check.json()["taken"] is not True:
-            print(f"{Fore.GREEN}[+] {user} is available{Fore.RESET}")
-        else:
-            print(f"{Fore.RED}{check.status_code} {user} is not available {Fore.RESET}")
+        if checked_user["errors"]["username"]["_errors"][0]["code"] == "USERNAME_ALREADY_TAKEN":
+            print(f"{Fore.RED}[+] {user} is not available {Fore.RESET}")
     except KeyError:
-        if check.status_code == 403 or check.status_code == 401:
-            print("Your request was denied, you need to have the name change option available for it to work!")
+        try:
+            if checked_user["errors"]["password"]:
+                print(f"{Fore.GREEN}[+] {user} is available {Fore.RESET}")
+        except KeyError:
+            if check.status_code == 403 or check.status_code == 401:
+                print(f"{Fore.RED}[-] Your request was denied, you need to have the name change option available for it to work! {Fore.RESET}")
 
 def main_menu() -> list:
     print("Welcome to Discord Username Checker")
@@ -45,7 +49,7 @@ def main_menu() -> list:
             how_many_to_check_for = int(input("How many random usernames do you want to check? >"))
             flag = True
         except ValueError:
-            print("Please enter a length of the letters and how many times as a whoe number\n\
+            print("Please enter a length of the letters and how many times as a whole number\n\
                   Example: 2 for 2 letter usernames, 100 if you want to check 100 random usernames")
     return [user_length, how_many_to_check_for]
 
@@ -58,4 +62,4 @@ if __name__ == "__main__":
         check_username(user)
         # Change this to your needs as no proxy support your requests will be blocked if spamming a lot so i recommend not putting too low
         sleep(0.8)
-    
+        
