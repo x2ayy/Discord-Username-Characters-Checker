3c4da7ab8e914ad529c71a68d38fc59fae042d2c
x2ayy <alexanderanghelides@outlook.com>
2023-06-30 21:27:27 +0100

    Changed from letters to letters and numbers.

--- checker.py
+++ checker.py
@@ -1,50 +1,42 @@
 from random import sample as rand_sample
 from string import ascii_letters, digits
-from requests import patch as req_patch
+from requests import post as req_post
 from time import sleep
 from colorama import Fore
-from json.decoder import JSONDecodeError
 
-
-BASE_URL = "https://discord.com/api/v9/users/@me"
+BASE_URL = "https://discord.com/api/v9/users/@me/pomelo-attempt"
 
 REQUEST_HEADERS = {
     "Content-Type": "Application/json",
     "Orgin": "https://discord.com/",
-    "Authorization":"YOUR-TOKEN-GOES-HERE"
+    "Authorization": "YOUR-TOKEN-GOES-HERE"
 }
 
 
 def generate_random_username(length: int) -> str:
-    return ''.join(rand_sample(ascii_letters + digits + "_" + ".", length))
+    characters = ascii_letters + digits  # Include both letters and digits
+    return ''.join(rand_sample(characters, length))
+
 
 def check_username(user: str):
     if 1 > len(user) < 33:
         print("Username must be 2-32 Characters")
         return
-    
-    user = user.lower()
-        
+
     data = {
         "username": user
     }
-    check = req_patch(BASE_URL, headers=REQUEST_HEADERS, json=data)
-    
-    try:
-        checked_user = check.json()
-        checked_user_code = checked_user["errors"]["username"]["_errors"][0]["code"]
+    check = req_post(BASE_URL, headers=REQUEST_HEADERS, json=data)
 
-        if checked_user_code == "USERNAME_ALREADY_TAKEN":
-            print(f"{Fore.RED}[+] {user} is not available {Fore.RESET}")
+    try:
+        if check.json()["taken"] is not True:
+            print(f"{Fore.GREEN}[+] {user} is available{Fore.RESET}")
         else:
-            print(f"{Fore.GREEN}[+] {user} is available {Fore.RESET}")
-    except JSONDecodeError:
-        print(f"{Fore.RED}[-] The JSON Content is a invalid format")
+            print(f"{Fore.RED}{check.status_code} {user} is not available {Fore.RESET}")
     except KeyError:
         if check.status_code == 403 or check.status_code == 401:
-            print(f"{Fore.RED}[-] Your request was denied, is your token correct? {Fore.RESET}")
-        else:
-            print(f"{Fore.RED}[DEBUG] Reponse: {check.status_code} Data: {checked_user} {Fore.RESET}")
+            print("REQUEST DENIED. Is the Discord token correct? Are you sending too many requests?")
+            return False
 
 
 def main_menu() -> list:
@@ -52,21 +44,20 @@ def main_menu() -> list:
     flag = False
     while flag is False:
         try:
-            user_length = int(input("How many letters in the username?  >"))
+            user_length = int(input("How many characters in the username?  >"))
             how_many_to_check_for = int(input("How many random usernames do you want to check? >"))
             flag = True
         except ValueError:
-            print("Please enter a length of the letters and how many times as a whole number\n\
-                  Example: 2 for 2 letter usernames, 100 if you want to check 100 random usernames")
+            print("Please enter the length of the characters and the number of usernames to check as whole numbers.\n\
+                  Example: 2 for 2 character usernames, 100 if you want to check 100 random usernames")
     return [user_length, how_many_to_check_for]
 
 
 if __name__ == "__main__":
-    username_letters, check_range = main_menu()
+    username_characters, check_range = main_menu()
 
     for i in range(check_range):
-        user = generate_random_username(username_letters)
+        user = generate_random_username(username_characters)
         check_username(user)
-        # Change this to your needs as no proxy support your requests will be blocked if spamming a lot so i recommend not putting too low
-        sleep(0.8)
-        
+        # Change this to your needs. As there is no proxy support, your requests may be blocked if spamming a lot, so I recommend not putting it too low.
+        sleep(1.2)
